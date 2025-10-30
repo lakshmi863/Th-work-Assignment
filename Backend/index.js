5dc5afefd09882e94f9d8f7fb97a401013459e34
lakshmi863 <sathya.lakshmipathi@gmail.com>
2025-10-30 15:30:57 +0530

    first commit chang

--- Backend/index.js
+++ Backend/index.js
@@ -6,10 +6,25 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Middleware
-app.use(cors()); 
+
+const allowedOrigins = ['https://th-work-task.onrender.com'];
+
+const corsOptions = {
+  origin: (origin, callback) => {
+    // Allows your frontend URL and tools like Postman (which have no origin)
+    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('This origin is not allowed by CORS'));
+    }
+  },
+  optionsSuccessStatus: 200 
+};
+
+// You MUST pass the options to cors() here
+app.use(cors(corsOptions));
+
 app.use(express.json()); 
-
-
 app.use('/tasks', taskRoutes); 
 
 app.listen(PORT, () => {
